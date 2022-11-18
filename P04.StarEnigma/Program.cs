828c7aebe51d5a614929d39bc1d299bda9508c5f
Nikolay Stoyanov <nikitig1987@abv.bg>
2022-11-18 16:02:33 +0100

    Add project files.

---
+++ P04.StarEnigma/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace P04.StarEnigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string encryptedMessage = Console.ReadLine();
+                string decryptedMessage = DecryptMessage(encryptedMessage);
+            }
+        }
+
+        static DecryptMessage(string encryptedMessage)
+        {
+            StringBuilder decryptedMessage = new StringBuilder();
+            int decryptionKey = GetDecryptionKey(encryptedMessage);
+
+            foreach (char currCh in encryptedMessage)
+            {
+                char decryptedCh = (char)(currCh - decryptionKey);
+                decryptedMessage.Append(decryptedCh);
+            }
+
+            return decryptedMessage.ToString();
+        }
+
+        static int GetDecryptionKey(string encryptedMessage)
+        {
+            string decryptPattern = "[star]{1}";
+            MatchCollection matches =
+                Regex.Matches(encryptedMessage, decryptPattern, RegexOptions.IgnoreCase);
+            return matches.Count;
+        }
+    }
+}
